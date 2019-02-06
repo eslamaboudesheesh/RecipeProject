919706dbc9bff1d923321d81f25e4ec2e5649dd5
eslam <eslamaboudesheesh@hotmail.com>
2019-02-06 07:26:23 +0200

    add  form to edite a recipe

--- src/app/recipes/recipe-edited/recipe-edited.component.ts
+++ src/app/recipes/recipe-edited/recipe-edited.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, FormControlName } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edited',
@@ -22,24 +22,44 @@ recipeFrom: FormGroup;
    this.initForm();
     });
   }
+  addNewIngerdientField () {
+    (<FormArray>this.recipeFrom.get('ingerdients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
   private initForm() {
     let recipename = ' ';
     let recipeDesc = ' ';
     let recipeImgPath = ' ';
+   const recipeIngerdient = new FormArray([]);
     if (this.editeMode) {
       const recipe = this.recipeSev.getRecipe(this.id);
       recipename = recipe.name;
       recipeDesc = recipe.description;
       recipeImgPath = recipe.imgPath;
+      if (recipe['ingredient']) {
+        for (const ingred of recipe.ingredient ) {
+          recipeIngerdient.push(
+            new FormGroup({
+              'name': new FormControl(ingred.name),
+              'amount': new FormControl(ingred.amount)
+            })
+          );
+        }
+      }
     }
 this.recipeFrom = new FormGroup({
   'name': new FormControl(recipename),
   'description': new FormControl(recipeDesc),
-  'imgPath' :  new FormControl(recipeImgPath)
+  'imgPath' :  new FormControl(recipeImgPath),
+  'ingerdients' : recipeIngerdient
 });
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.recipeFrom);
   }
 
