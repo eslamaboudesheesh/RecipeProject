cb6fdaf8a1bef9c88de2e43f8199275b534ad432
eslam <eslamaboudesheesh@hotmail.com>
2019-02-06 07:47:01 +0200

    add validat to reactive form

--- src/app/recipes/recipe-edited/recipe-edited.component.ts
+++ src/app/recipes/recipe-edited/recipe-edited.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
-import { FormGroup, FormControl, FormArray, FormControlName } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, FormControlName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edited',
@@ -25,8 +25,8 @@ recipeFrom: FormGroup;
   addNewIngerdientField () {
     (<FormArray>this.recipeFrom.get('ingerdients')).push(
       new FormGroup({
-        'name': new FormControl(),
-        'amount': new FormControl()
+        'name': new FormControl(null , Validators.required ),
+        'amount': new FormControl(null , [ Validators.required , Validators.pattern(/^[1-9]+[0-9]*$/) ])
       })
     );
   }
@@ -44,17 +44,17 @@ recipeFrom: FormGroup;
         for (const ingred of recipe.ingredient ) {
           recipeIngerdient.push(
             new FormGroup({
-              'name': new FormControl(ingred.name),
-              'amount': new FormControl(ingred.amount)
+              'name': new FormControl(ingred.name , Validators.required),
+              'amount': new FormControl(ingred.amount , [ Validators.required , Validators.pattern(/^[1-9]+[0-9]*$/) ])
             })
           );
         }
       }
     }
 this.recipeFrom = new FormGroup({
-  'name': new FormControl(recipename),
-  'description': new FormControl(recipeDesc),
-  'imgPath' :  new FormControl(recipeImgPath),
+  'name': new FormControl(recipename, Validators.required),
+  'description': new FormControl(recipeDesc, Validators.required),
+  'imgPath' :  new FormControl(recipeImgPath , Validators.required),
   'ingerdients' : recipeIngerdient
 });
   }
